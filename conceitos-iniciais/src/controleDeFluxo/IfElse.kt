f964a7f97a6681f816f1ac45266c0fa335887658
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-12 03:08:47 -0300

    If Else

--- conceitos-iniciais/src/controleDeFluxo/IfElse.kt
+++ conceitos-iniciais/src/controleDeFluxo/IfElse.kt
@@ -1,5 +1,8 @@
 package controleDeFluxo
 
+/*
+* O if e else é igual ao usado no Java
+* */
 fun maiorDeIdade(idade: Int) {
     if (idade >= 18) {
         println("Maior de idade!")
@@ -8,10 +11,27 @@ fun maiorDeIdade(idade: Int) {
     }
 }
 
+fun calculaBonus(cargo: String, salario: Float): Float {
+    // Gerente Junior, Gerente Senior, Coordenador
+    val bonus: Float
+    if (cargo == "Coordenador") {
+        bonus = salario * 0.2f
+    } else if (cargo == "Gerente Junior") {
+        bonus = salario * 0.5f
+    } else {
+        bonus = salario * 2
+    }
+    return bonus
+}
+
 // Obs.: Para identar o códio no mac é command + alt 'L'
 
 fun main() {
     maiorDeIdade(10)
+    maiorDeIdade(17)
+    maiorDeIdade(22)
+    println("--------------------------")
+    println(calculaBonus("Coordenador", 2500.00f))
+    println(calculaBonus("Gerente Junior", 2500.00f))
+    println(calculaBonus("Gerente Senior", 2500.00f))
 }
-
-// PAREI NO MINUTO 3:5 DA AULA 21
